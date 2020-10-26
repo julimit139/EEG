5e3af5cdc6f86f0ec30658047abfeb07f0c8bb2d
julimit139 <32513997+julimit139@users.noreply.github.com>
2020-10-26 17:20:39 +0100

    Added file for storing calculating thresholds for artifact detection methods (thresholdCalculation.py).

---
+++ thresholdCalculation.py
@@ -0,0 +1,7 @@
+import numpy as np
+import statistics
+
+# 1
+# function calculating thresholds for External Electrostatic Potentials - EEP (7.5.1)
+def calculateThresholdsEEP(xmin, xmax, ):
+    pass
