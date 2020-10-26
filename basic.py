36ebd6180aa50777975effca1d83ee49521bfb06
julimit139 <32513997+julimit139@users.noreply.github.com>
2020-10-26 11:17:09 +0100

    Revert "Added basic reading edf file byte by byte (main.py)."
    This reverts commit d8ba16ce

--- basic.py
+++ basic.py
@@ -1,19 +1,15 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import mne
 
+# loading asc file into numpy array (ndarray)
+ascArr = np.loadtxt("test.asc", skiprows=11)
+print(ascArr)
 
-# loading asc file into numpy array - ndarray
-ascArray = np.loadtxt("Data/test.asc", skiprows=11)
-print(ascArray)
-print("Shape of ascArray: ")
-print(ascArray.shape)
-
-print("\n\n")
-
-# loading data from 1 s (128 samples) from channel Fp1 to ndarray
-Fp1 = np.array(ascArray[:128, 1])
-print(Fp1.shape)
-
-# signal plotting
-plt.plot(Fp1)
-plt.show()
+"""
+# loading edf+ file into raw data
+file = "C:/Users/Julia/Desktop/EDF/test.edf"
+data = mne.io.read_raw_edf(file)
+raw_data = data.get_data()
+info = data.info
+channels = data.ch_names
+"""
