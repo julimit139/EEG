f688a1252aa39eab46f961b0187df7a5bc0004c5
julimit139 <32513997+julimit139@users.noreply.github.com>
2020-10-26 21:35:45 +0100

    Added basic parts of function ``calculateThresholdsEEP`` (thresholdCalculation.py).

--- thresholdCalculation.py
+++ thresholdCalculation.py
@@ -1,7 +1,34 @@
 import numpy as np
 import statistics
+import math
 
-# 1
+# functions calculating median and standard deviation for calculating thresholds of EEP
+def calculateMedian(array):
+    median = statistics.median(array)
+    return median
+
+
+def calculateStandardDeviation(array):
+    standardDeviation = statistics.stdev(array)
+    return standardDeviation
+
+
+# 1st
 # function calculating thresholds for External Electrostatic Potentials - EEP (7.5.1)
-def calculateThresholdsEEP(xmin, xmax, ):
-    pass
+def calculateThresholdsEEP(minmaxArray):
+    minArray = []
+    maxArray = []
+    for tup in range(len(minmaxArray)):
+        xmin, xmax = tup
+        xminNorm = math.log(abs(xmin), 10)
+        xmaxNorm = math.log((xmax, 10))
+        minArray.append(xminNorm)
+        maxArray.append(xmaxNorm)
+
+    minMedian = calculateMedian(minArray)
+    maxMedian = calculateMedian(maxArray)
+
+    minStandardDeviation = calculateStandardDeviation(minArray)
+    maxStandardDeviation = calculateStandardDeviation(maxArray)
+
+    minThreshold = minMedian + 6 * minStandardDeviation
