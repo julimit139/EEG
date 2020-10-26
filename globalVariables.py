259fc529415b170c4985e8226e6ffabac45d745d
julimit139 <32513997+julimit139@users.noreply.github.com>
2020-10-26 17:18:31 +0100

    Added file for storing global variables (globalVariables.py).

---
+++ globalVariables.py
@@ -0,0 +1,8 @@
+# here are stored global variables describing data being analyzed
+
+# in the future these values can be read from asc/edf file directly here
+
+samplingRate = 128      # sampling rate used in examination
+second = 128            # number of rows in array equal to one second of examination data
+eegChannelNumber = 20   # number of EEG channels in examination data
+ekgChannelNumber = 1    # number of EKG channels in examination data
