e3da9f2c75a2af6aae3465692d764b982c44f39d
julimit139 <32513997+julimit139@users.noreply.github.com>
2020-10-26 11:20:01 +0100

    Removed first printing actions added right after creating the project. Added basic reading edf file byte by byte (main.py).

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-print("Wow! Added first line of code in EEG project!")
-print("Wow! Addd second line of code in EEG project!")
-print("Wow! And another line of code added on laptop!")
+import numpy as np
+import os
+
+# reading edf file
+fileName = "C:/Users/Julia/Desktop/EDF/KT53.edf"
+file = open(fileName, "rb")
+fileSize = os.path.getsize(fileName)
+# print(fileSize)
+edfList = []
+for i in range(30000):
+    edfList.append(file.read(1))
+    print(edfList[i])
+file.close()
+
+arr = np.array(edfList)
+print(arr)
+file = open("Data/KT53.txt", "wb")
+file.write(arr)
+file.close()
