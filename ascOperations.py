ed046fa1653fc2b9b7e1bbf72b3de93a5de2306e
Julia Mitas <juliamitas.lote@gmail.com>
2020-11-06 21:32:28 +0100

    Made some unimportant changes in order to test program (ascOperations.py).

--- ascOperations.py
+++ ascOperations.py
@@ -8,20 +8,28 @@ inputData = np.loadtxt("C:/Users/Julia/Desktop/Data/test.asc", skiprows=11)
 print(inputData)
 print("Shape of input: ")
 print(inputData.shape)
+print(inputData.ndim)
 
 print("\n\n")
 
-# loading data from 1 s (128 samples) from first channel to ndarray
-channel = np.array(inputData[165504:165632, 1])
+# loading data from 1 s from first channel to ndarray (column with index 0 in inputData contains EKG signal)
+channel = np.array(inputData[:, 2])
+print(channel)
 print("Channel shape: ")
 print(channel.shape)
+print(channel.ndim)
+
+
+print(artifactDetection.detectEEP(channel))
+
+
 
 
 # signal plotting
 # col = np.where(Fp1 < 100, 'red', 'green')
 # print("Col shape: ")
 # print(col.shape)
-plt.plot(channel, linewidth=0.2)
+"""plt.plot(channel, linewidth=0.2)
 plt.xlabel("Number of samples")
 plt.ylabel("Potential value")
-plt.show()
+plt.show()"""
