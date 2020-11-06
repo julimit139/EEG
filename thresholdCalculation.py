a6205b8103116078026e74c67ddf3fd837393e41
Julia Mitas <juliamitas.lote@gmail.com>
2020-11-06 21:31:46 +0100

    Finished working on ``calculateThresholdsEEP`` function (thresholdCalculation.py).

--- thresholdCalculation.py
+++ thresholdCalculation.py
@@ -2,33 +2,63 @@ import numpy as np
 import statistics
 import math
 
+
 # functions calculating median and standard deviation for calculating thresholds of EEP
-def calculateMedian(array):
-    median = statistics.median(array)
+def calculateMedian(calcList):
+    median = statistics.median(calcList)
     return median
 
 
-def calculateStandardDeviation(array):
-    standardDeviation = statistics.stdev(array)
+def calculateStandardDeviation(calcList):
+    standardDeviation = statistics.stdev(calcList)
     return standardDeviation
 
 
 # 1st
 # function calculating thresholds for External Electrostatic Potentials - EEP (7.5.1)
-def calculateThresholdsEEP(minmaxArray):
-    minArray = []
-    maxArray = []
-    for tup in range(len(minmaxArray)):
-        xmin, xmax = tup
-        xminNorm = math.log(abs(xmin), 10)
-        xmaxNorm = math.log((xmax, 10))
-        minArray.append(xminNorm)
-        maxArray.append(xmaxNorm)
+"""
+Calculates thresholds for External Electrostatic Potentials (EEP) detection function.
+Called inside the ``detectEEP`` function. 
+Parameters:
+    minMaxList : list
+        List of tuples containing minimum and maximum signal values from each time block of a channel.
+Returns:
+    thresholds : tuple 
+        Tuple containing minimum and maximum thresholds values for channel on which detection function is called. 
+"""
 
-    minMedian = calculateMedian(minArray)
-    maxMedian = calculateMedian(maxArray)
 
-    minStandardDeviation = calculateStandardDeviation(minArray)
-    maxStandardDeviation = calculateStandardDeviation(maxArray)
+def calculateThresholdsEEP(minMaxList):
+    # creating lists to contain normalised min and max values from all blocks
+    minList = []
+    maxList = []
 
+    # iterating through minMaxList and filling minList and maxList with normalised values
+    for (xmin, xmax) in minMaxList:
+        xMin = xmin.item()
+        xMax = xmax.item()
+        if xMin != 0 and xMax != 0:
+            xMinNorm = math.log(abs(xMin), 10)
+            xMaxNorm = math.log(xMax, 10)
+            minList.append(xMinNorm)
+            maxList.append(xMaxNorm)
+
+    # calculating median values in minList and maxList
+    minMedian = calculateMedian(minList)
+    maxMedian = calculateMedian(maxList)
+
+    # calculating standard deviation values in minList and maxList
+    minStandardDeviation = calculateStandardDeviation(minList)
+    maxStandardDeviation = calculateStandardDeviation(maxList)
+
+    # calculating values of minThreshold and maxThreshold
     minThreshold = minMedian + 6 * minStandardDeviation
+    maxThreshold = maxMedian + 6 * maxStandardDeviation
+
+    # creating tuple containing threshold values
+    thresholds = (minThreshold, maxThreshold)
+
+    # returning thresholds
+    return thresholds
+
+
