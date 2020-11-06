9c9a470c7a6efeec8ef6e38826cf65851b86a880
Julia Mitas <juliamitas.lote@gmail.com>
2020-11-06 13:58:32 +0100

    Changed comments.

--- globalVariables.py
+++ globalVariables.py
@@ -1,7 +1,7 @@
 # Here are stored global variables describing data being analyzed
-# In the future these values will be read from asc/edf file directly here
+# In the future these values will be read from input file directly here
 
-examinationTime = 1294  # duration of examination in seconds
-samplingRate = 128      # sampling rate used in examination
+examinationTime = 1294  # duration of examination (in s)
+samplingRate = 128      # sampling rate used in examination (in Hz)
 eegChannelNumber = 20   # number of EEG channels in examination data
 ekgChannelNumber = 1    # number of EKG channels in examination data
