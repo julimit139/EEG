a547a181a376d9b06a32b631d2f3ee678152ee65
Julia Mitas <juliamitas.lote@gmail.com>
2020-11-06 13:57:56 +0100

    'ascArray' refactored into 'inputData'. 'Fp1' refactored into 'channel'.

--- ascOperations.py
+++ ascOperations.py
@@ -3,27 +3,25 @@ import matplotlib.pyplot as plt
 import artifactDetection
 import thresholdCalculation
 
-# loading asc file into numpy array - ndarray
-ascArray = np.loadtxt("C:/Users/Julia/Desktop/Data/test.asc", skiprows=11)
-print(ascArray)
-print("Shape of ascArray: ")
-print(ascArray.shape)
+# loading asc file into numpy array (ndarray) -> it'll load file given through GUI (later: should change '\' into '/')
+inputData = np.loadtxt("C:/Users/Julia/Desktop/Data/test.asc", skiprows=11)
+print(inputData)
+print("Shape of input: ")
+print(inputData.shape)
 
 print("\n\n")
 
-# loading data from 1 s (128 samples) from channel Fp1 to ndarray
-Fp1 = np.array(ascArray[165504:165632, 1])
-print("Fp1 shape: ")
-print(Fp1.shape)
-
+# loading data from 1 s (128 samples) from first channel to ndarray
+channel = np.array(inputData[165504:165632, 1])
+print("Channel shape: ")
+print(channel.shape)
 
 
 # signal plotting
-
 # col = np.where(Fp1 < 100, 'red', 'green')
 # print("Col shape: ")
 # print(col.shape)
-plt.plot(Fp1, linewidth=0.2)
+plt.plot(channel, linewidth=0.2)
 plt.xlabel("Number of samples")
 plt.ylabel("Potential value")
 plt.show()
