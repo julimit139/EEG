1c0d8f1467c2a8af17dd215b22c4125732bc35a0
Julia Mitas <juliamitas.lote@gmail.com>
2020-11-16 13:27:19 +0100

    Added reading global variables values from input file.

--- globalVariables.py
+++ globalVariables.py
@@ -1,7 +1,31 @@
 # Here are stored global variables describing data being analyzed
 # In the future these values will be read from input file directly here
+# Header lines in asc file end with spaces
 
-examinationTime = 1294  # duration of examination (in s)
-samplingRate = 128      # sampling rate used in examination (in Hz)
+inputFile = open("C:/Users/Julia/Desktop/Data/test.asc", "r")
+for i in range(20):
+    line = inputFile.readline()
+
+    if "Seconds" in line:
+        examinationTime = ""
+        for i in line:
+            if i.isdigit():
+                examinationTime += i
+        print(examinationTime)
+
+    if "Sampling rate" in line:
+        samplingRate = ""
+        for i in line:
+            if i.isdigit():
+                samplingRate += i
+        print(samplingRate)
+
+    if "EKG" in line:
+        pass
+
+
+
+# examinationTime = 1294  # duration of examination (in s)
+# samplingRate = 128      # sampling rate used in examination (in Hz)
 eegChannelNumber = 20   # number of EEG channels in examination data
 ekgChannelNumber = 1    # number of EKG channels in examination data
