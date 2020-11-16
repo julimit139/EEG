563ca33d04e84606622c87c79dab464bbd5c4dec
Julia Mitas <juliamitas.lote@gmail.com>
2020-11-16 15:07:42 +0100

    Commented major part of code.

--- globalVariables.py
+++ globalVariables.py
@@ -2,7 +2,7 @@
 # In the future these values will be read from input file directly here
 # Header lines in asc file end with spaces
 
-inputFile = open("C:/Users/Julia/Desktop/Data/test.asc", "r")
+"""inputFile = open("C:/Users/Julia/Desktop/Data/test.asc", "r")
 for i in range(20):
     line = inputFile.readline()
 
@@ -22,10 +22,10 @@ for i in range(20):
 
     if "EKG" in line:
         pass
+"""
 
 
-
-# examinationTime = 1294  # duration of examination (in s)
-# samplingRate = 128      # sampling rate used in examination (in Hz)
+examinationTime = 1294  # duration of examination (in s)
+samplingRate = 128      # sampling rate used in examination (in Hz)
 eegChannelNumber = 20   # number of EEG channels in examination data
 ekgChannelNumber = 1    # number of EKG channels in examination data
