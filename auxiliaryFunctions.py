e45fa45377edb55d37811e9c335998fd2cd3e763
Julia Mitas <juliamitas.lote@gmail.com>
2020-11-18 11:52:23 +0100

    Added some parts of `calculateFourier` function. Need some more thinking.

--- auxiliaryFunctions.py
+++ auxiliaryFunctions.py
@@ -1,4 +1,7 @@
 import statistics
+import numpy as np
+from scipy.fft import fft
+import scipy.integrate as integrate
 import globalVariables as gV
 
 
@@ -14,5 +17,26 @@ def calculateStandardDeviation(calcList):
     return standardDeviation
 
 
-def calculateFourierTransforms(calcList):
-    pass
+# function calculating square modulus of a Fourier Transform array
+def calculateSquareModulus(calcList):
+    inputData = fft(calcList)
+    result = np.abs(inputData)
+    result **= 2
+    return result
+
+
+def callAnything(calcList):
+    return calcList
+
+
+def calculateFourier(calcList):
+    # squareModulus = calculateSquareModulus(calcList)
+    # numerator = integrate.quad(calculateSquareModulus(calcList), 0, gV.lambdaFrequency)
+    # denominator = integrate.quad(calculateSquareModulus(calcList), 0, gV.nyquistFrequency) - integrate.quad(calculateSquareModulus(calcList), gV.electricFrequency-2, gV.electricFrequency+2)
+    # numerator = integrate.quad(func=callAnything, a=0, b=gV.lambdaFrequency, args=calcList)[0]
+    # denominator = integrate.quad(func=callAnything, a=0, b=gV.nyquistFrequency, args=calcList)[0] - integrate.quad(func=callAnything, a=gV.electricFrequency - 2, b=gV.electricFrequency + 2, args=calcList)[0]
+    numerator = integrate.simps(calculateSquareModulus(calcList))
+    denominator = integrate.simps(calculateSquareModulus(calcList)) - integrate.simps(
+        calculateSquareModulus(calcList))
+    result = float(numerator / denominator)
+    return result
