0aafd1d1526ad0d4a5bc4d0975954f88377e7e30
Julia Mitas <juliamitas.lote@gmail.com>
2020-11-19 16:53:23 +0100

    Still working on calculateFourier.

--- auxiliaryFunctions.py
+++ auxiliaryFunctions.py
@@ -25,18 +25,18 @@ def calculateSquareModulus(calcList):
     return result
 
 
-def callAnything(calcList):
+def retCalcList(calcList):
     return calcList
 
 
 def calculateFourier(calcList):
     # squareModulus = calculateSquareModulus(calcList)
-    # numerator = integrate.quad(calculateSquareModulus(calcList), 0, gV.lambdaFrequency)
-    # denominator = integrate.quad(calculateSquareModulus(calcList), 0, gV.nyquistFrequency) - integrate.quad(calculateSquareModulus(calcList), gV.electricFrequency-2, gV.electricFrequency+2)
-    # numerator = integrate.quad(func=callAnything, a=0, b=gV.lambdaFrequency, args=calcList)[0]
-    # denominator = integrate.quad(func=callAnything, a=0, b=gV.nyquistFrequency, args=calcList)[0] - integrate.quad(func=callAnything, a=gV.electricFrequency - 2, b=gV.electricFrequency + 2, args=calcList)[0]
-    numerator = integrate.simps(calculateSquareModulus(calcList))
-    denominator = integrate.simps(calculateSquareModulus(calcList)) - integrate.simps(
-        calculateSquareModulus(calcList))
-    result = float(numerator / denominator)
-    return result
+    numerator = integrate.quad(calculateSquareModulus(calcList), 0, gV.lambdaFrequency)
+    denominator = integrate.quad(calculateSquareModulus(calcList), 0, gV.nyquistFrequency) - integrate.quad(calculateSquareModulus(calcList), gV.electricFrequency-2, gV.electricFrequency+2)
+    # numerator = integrate.quad(func=retCalcList, a=0, b=gV.lambdaFrequency, args=calcList)[0]
+    # denominator = integrate.quad(func=retCalcList, a=0, b=gV.nyquistFrequency, args=calcList)[0] - integrate.quad(func=retCalcList, a=gV.electricFrequency - 2, b=gV.electricFrequency + 2, args=calcList)[0]
+    # numerator = integrate.simps(calculateSquareModulus(calcList))
+    # denominator = integrate.simps(calculateSquareModulus(calcList)) - integrate.simps(
+       # calculateSquareModulus(calcList))
+    # result = float(numerator / denominator)
+    return numerator, denominator
