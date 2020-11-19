bb8acdedf94966e52a58a95b82911e70859b3ba7
Julia Mitas <juliamitas.lote@gmail.com>
2020-11-19 16:53:48 +0100

    A mess. Tested many things.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import artifactDetection as aD
 import globalVariables as gV
 import auxiliaryFunctions as aF
 import dataExtraction as eD
+import graphPlotting as gP
 
 
 # loading asc file into numpy array (ndarray) -> it'll load file given through GUI (later: should change '\' into '/')
@@ -13,7 +14,11 @@ inputData = np.loadtxt("C:/Users/Julia/Desktop/Data/test.asc", skiprows=11)
 # print(inputData.shape)
 # print(inputData.ndim)
 
-# channel = np.array(inputData[165504:165632, 1])
+channel = np.array(inputData[165504:165632, 1])
+print(channel)
+print(aF.calculateSquareModulus(channel))
+# print(aF.calculateFourier(channel))
+
 
 print(aD.performDetection(aD.detectEEP, inputData))
 
@@ -23,6 +28,7 @@ arr = np.array(inputData[165120:165632, 1:20])
 channels = np.swapaxes(arr, 0, 1)
 print(channels)
 print(channels.shape)
+
 
 """
 x = np.arange(0, 512, 1)
@@ -38,37 +44,12 @@ ax.set_yticks([-100, 0, 100])
 plt.show()
 """
 
-x = np.arange(0, 512, 1)
-fig = plt.figure()
-ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
-y = channels
-ax.plot(x, channels.T + 900*np.arange(18, -1, -1))
-ax.plot(np.zeros((512, 19)) + 900*np.arange(18, -1, -1), '--', color='gray')
-ax.set_xlabel('Time of examination [s]')
-ax.set_title('EEG')
-ax.set_xticks([0, 128, 256, 384, 512])
-ax.set_xticklabels([0, 1, 2, 3, 4])
-ax.set_yticks([-100, 0, 100])
+# creating results directory from its path
+resultsDirectory = gP.createPath()
+gP.createDirectory(resultsDirectory)
 
-# plt.plot(channels.T + 900*np.arange(18, -1, -1))
-
-# plt.plot(np.zeros((512, 19)) + 900*np.arange(18, -1, -1), '--', color='gray')
-
-
-fig.canvas.manager.window.showMaximized()
-plt.show()
-# plt.yticks([])
-
-# plt.legend(channelNames, loc='upper right', labelspacing=1.6, title='Channels')
-
-# plt.axis('tight')
-
-# plt.show()
-
-
-
-
-
+# gP.plotBlock(channels, resultsDirectory)
+gP.plotAllBlocks(inputData, resultsDirectory)
 
 
 
