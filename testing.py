fcbe5c47bad105c1f0fb17571c43b7fdcc0c35a2
Julia Mitas <juliamitas.lote@gmail.com>
2020-11-19 22:47:50 +0100

    Added more testing.

--- testing.py
+++ testing.py
@@ -1,3 +1,34 @@
-import graphPlotting as gP
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.fft as fft
+import auxiliaryFunctions as aF
+import artifactDetection as aD
+import globalVariables as gV
 
-print(gP.createPath())
+
+inputData = np.loadtxt("C:/Users/Julia/Desktop/Data/test.asc", skiprows=11)
+channel = np.array(inputData[:, 1])
+
+for channelNumber in range(gV.eegChannelNumber):
+    channel = np.array(inputData[:, channelNumber+1])
+    result = aD.detectLFP(channel)
+    print("Channel number: " + str(channelNumber+1))
+    print(len(result))
+    try:
+        print(result.index(True))
+    except ValueError:
+        print("True is not in list")
+
+
+
+"""
+fourierTransform = fft(channel)
+
+absFourierTransform = np.abs(fourierTransform)
+
+powerSpectrum = np.square(absFourierTransform)
+
+frequency = np.linspace(0, gV.samplingRate/2, len(powerSpectrum))
+
+print(sum(powerSpectrum[0:5]))
+"""
