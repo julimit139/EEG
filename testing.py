4e71e4f89a80a8d84810cfbaf6bc6d209107bfc2
Julia Mitas <juliamitas.lote@gmail.com>
2020-11-19 16:54:16 +0100

    Added some testing.

--- testing.py
+++ testing.py
@@ -1,19 +1,3 @@
-"""import matplotlib.pyplot as plt
+import graphPlotting as gP
 
-valueX = [1, 2, 3, 4]
-scoreList = [5, 0, 0, 2]
-plt.plot(valueX, scoreList)
-plt.xticks(range(min(valueX), max(valueX)+1))
-plt.xlabel("Score number")
-plt.ylabel("Score")
-plt.show()"""
-
-import numpy as np
-import matplotlib.pyplot as plt
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-x = np.arange(0, 10, 0.2)
-y = [2.5, 3, 1.5, ... , 7, 9]
-ax.plot(x, y)
-plt.show()
+print(gP.createPath())
