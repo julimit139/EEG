3c5a18612e5ab242eb1ff2729eb67b8d6a1a15e0
Julia Mitas <juliamitas.lote@gmail.com>
2020-11-19 22:47:24 +0100

    Added more to `calculateFourierFunction` and changed `calculateFourierSquareModulus` function.

--- auxiliaryFunctions.py
+++ auxiliaryFunctions.py
@@ -18,25 +18,26 @@ def calculateStandardDeviation(calcList):
 
 
 # function calculating square modulus of a Fourier Transform array
-def calculateSquareModulus(calcList):
-    inputData = fft(calcList)
-    result = np.abs(inputData)
-    result **= 2
-    return result
+def calculateFourierSquareModulus(calcList):
+    fourier = fft(calcList)
+    fourierModulus = np.abs(fourier)
+    fourierSquareModulus = np.square(fourierModulus)
+    return fourierSquareModulus
 
 
-def retCalcList(calcList):
-    return calcList
+# function calculating Fourier based function value (book: formula 6.3) in one channel in one block
+def calculateFourierFunction(calcList):
+    powerSpectrum = calculateFourierSquareModulus(calcList)
+    frequency = np.linspace(0, gV.samplingRate/2, len(powerSpectrum))
 
+    nominatorLimit = np.where(frequency < gV.lambdaFrequency)
+    nominator = sum(powerSpectrum[nominatorLimit])
 
-def calculateFourier(calcList):
-    # squareModulus = calculateSquareModulus(calcList)
-    numerator = integrate.quad(calculateSquareModulus(calcList), 0, gV.lambdaFrequency)
-    denominator = integrate.quad(calculateSquareModulus(calcList), 0, gV.nyquistFrequency) - integrate.quad(calculateSquareModulus(calcList), gV.electricFrequency-2, gV.electricFrequency+2)
-    # numerator = integrate.quad(func=retCalcList, a=0, b=gV.lambdaFrequency, args=calcList)[0]
-    # denominator = integrate.quad(func=retCalcList, a=0, b=gV.nyquistFrequency, args=calcList)[0] - integrate.quad(func=retCalcList, a=gV.electricFrequency - 2, b=gV.electricFrequency + 2, args=calcList)[0]
-    # numerator = integrate.simps(calculateSquareModulus(calcList))
-    # denominator = integrate.simps(calculateSquareModulus(calcList)) - integrate.simps(
-       # calculateSquareModulus(calcList))
-    # result = float(numerator / denominator)
-    return numerator, denominator
+    denominatorLimitFirst = np.where(frequency < gV.nyquistFrequency)
+    denominatorLimitSecond = np.where((frequency > (gV.electricFrequency-2)) & (frequency < (gV.electricFrequency+2)))
+    denominator = sum(powerSpectrum[denominatorLimitFirst]) - sum(powerSpectrum[denominatorLimitSecond])
+
+    fourierFunction = nominator / denominator
+
+    return fourierFunction
+
