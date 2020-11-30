acff7b3735d86366e92d4fa60bc340f6b11d30b8
Julia Mitas <juliamitas.lote@gmail.com>
2020-11-30 15:20:47 +0100

    Needs improvement.

--- globalVariables.py
+++ globalVariables.py
@@ -6,12 +6,14 @@
 examinationTime = 1294                  # duration of examination (in s)
 samplingRate = 128                      # sampling rate used in examination (in Hz)
 nyquistFrequency = samplingRate / 2     # Nyquist frequency - half of samplingRate (in Hz)
-electricFrequency = 50                  # electric network frequency in Europe (in Hz)
-lambdaFrequency = 0.625                 # frequency necessary for detecting LFP artifacts (in Hz)
+
 eegChannelNumber = 20                   # number of EEG channels in examination data
 ekgChannelNumber = 1                    # number of EKG channels in examination data
+
+electricFrequency = 50                  # electric network frequency in Europe (in Hz)
+lambdaFrequency = 0.625                 # frequency necessary for detecting LFP artifacts (in Hz)
 
 channelNames = ['Fp1-RF', 'Fp2-RF', 'F7-RF', 'F3-RF', 'Fz-RF', 'F4-RF', 'F8-RF', 'T3-RF', 'C3-RF', 'Cz-RF', 'C4-RF',
                 'T4-RF', 'T5-RF', 'P3-RF', 'Pz-RF', 'P4-RF', 'T6-RF', 'O1-RF', 'O2-RF']
 
-artifactBackgroundColor = 'lightcoral'  # name of colour set to axes when an artifact is detected in a block
+# artifactBackgroundColor = 'lightcoral'  # name of colour set to axes when an artifact is detected in a block
