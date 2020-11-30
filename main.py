6f48363c4860315666c4deaa241d526b4c2bacc0
Julia Mitas <juliamitas.lote@gmail.com>
2020-11-30 15:21:27 +0100

    Just main.

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ import graphPlotting as gP
 inputData = np.loadtxt("C:/Users/Julia/Desktop/Data/test.asc", skiprows=11)
 
 result = aD.performEEPDetection(inputData)
+# result = aD.performECGDetection(inputData)
+# result = aD.performLFPDetection(inputData)
+
 isArtifact = result[0]
 message = result[1]
+artifactNumber = isArtifact.count(True)
+print(artifactNumber)
+
+isArtifactArray = np.array(isArtifact)
+print(np.where(isArtifactArray == True))
 
 gP.plotAllBlocks(inputData, isArtifact, message)
 
